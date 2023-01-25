3047902078db47b0001e1fdaf1ff7a088532e088
ElricFoong <elricfoong@gmail.com>
2023-01-25 20:21:25 +1100

    add sum_length

--- read.py
+++ read.py
@@ -1,9 +1,13 @@
 data = []
-counter = 0
-with open("reviews.txt", "r")as f:
-    for line in f:
-        counter += 1
-        data.append(line)
-        if counter % 1000 == 0:
+count = 0
 
+with open("reviews.txt", "r") as f:
+    for line in f:
+        count += 1
+        data.append(line)
+        if count % 1000 == 0:
             print(len(data))
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print("average comment of length are ", sum_len/len(data))
