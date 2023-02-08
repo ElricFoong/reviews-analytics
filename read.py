47245c45ece51bc5e2c6a5808bfabfe0c703d07f
ElricFoong <elricfoong@gmail.com>
2023-02-08 21:54:35 +1100

    add progress bar

--- read.py
+++ read.py
@@ -1,54 +1,29 @@
-def read_file(filename):
-    count = 0
-    data = []
-    with open(filename, "r", encoding='utf-8')as f:
-        for line in f:
-            count += 1
-            data.append(line.strip())
-    return data
+import time
+import progressbar
+bar = progressbar.ProgressBar(max_value=1000000)
+data = []
+count = 0
+with open('reviews.txt', 'r')as f:
+    for lines in f:
+        count += 1
+        data.append(lines.strip())
+        bar.update(count)
+        # print(len(data))
 
-# 文字计数
-
-
-def dic(data):
-    wc = {}
-    for lines in data:
-        words = lines.split()
-        for word in words:
-            if word in wc:
-                wc[word] += 1
-            else:
-                wc[word] = 1  # 新增新的key进wc字典
-
-    return wc
-
-
-def word_check(data):
-    for word in data:
-        if data[word] > 100000:
-            print(word, data[word])
-
-
-def word_check2(data):
-    while True:
-        name = input('请输入想要查的字: ')
-        if name == 'q':
-            break
-
-        if name in data:
-            print(name, data[name])
+wc = {}
+for lines in data:
+    word = lines.split()
+    for word in word:
+        if word in wc:
+            wc[word] += 1
         else:
-            print('你所打的字并不存在 ')
+            wc[word] = 1
 
-    print('感谢使用查询功能. ')
-
-
-def main():
-    filename = 'reviews.txt'
-    data = read_file(filename)
-    data = dic(data)
-    word_check(data)
-    word_check2(data)
-
-
-main()
+while True:
+    user = input(' type ')
+    if user == 'q':
+        break
+    if user in wc:
+        print(user, wc[user])
+    else:
+        print('not available')
